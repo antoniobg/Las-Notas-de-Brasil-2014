623f357e7699ef8bfc2988d8f83cec1e7a9e8cdd
Antonio Borrero Granell <abogra1987@gmail.com>
2014-06-10 23:16:04 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brazil2014::Application.config.secret_key_base = 'eebdff3660eb699322938c15043051d0a8f5453c60c6f2c84cfe1fb63ec2cf81ef156d130202ca42fb27b85b75e2a191785e72e7893ca5b37c892248c3fb583c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brazil2014::Application.config.session_store :cookie_store, key: '_Brazil2014_session'
