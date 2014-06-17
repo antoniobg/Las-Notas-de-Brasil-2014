e267ba91095b0f095db3281279e60514333e810c
Antonio Borrero Granell <abogra1987@gmail.com>
2014-06-17 13:52:35 +0200

    Change links for games when user is admin

--- app/assets/javascripts/players.js
+++ app/assets/javascripts/players.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
   $('#player_position_id').select2();
   $('#performance_record_player_id').select2();
-  $(function(){
-    $("#player-games-table").tablesorter(
-      {
-        theme: 'bootstrap',
-        headerTemplate: '{content} {icon}',
-        widgets : [ 'zebra', 'columns', 'uitheme' ]
-        });
-  });
+  $("#player-games-table").tablesorter(
+    {
+      theme: 'bootstrap',
+      headerTemplate: '{content} {icon}',
+      widgets : [ 'zebra', 'columns', 'uitheme' ]
+      });
 });
